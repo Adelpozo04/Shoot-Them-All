e87731775a7384d57616db957351c0435ecb7e38
Adelpozo04 <addelp01@ucm.es>
2023-06-02 16:39:20 +0200

    Disparo 2.0
    Meter enfriamiento entre bala y bala
    Iniciar componente de movimiento de la bala recta (solo crear el script)

---
+++ Shoot Them All/Assets/Scripts/Combat/BalaRectaComponentç.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalaRectaComponentç : MonoBehaviour
+{
+
+    #region parameters
+
+    [SerializeField] private float _velocity;
+
+    #endregion
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
