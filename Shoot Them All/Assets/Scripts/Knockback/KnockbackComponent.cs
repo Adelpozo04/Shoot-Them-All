c1671c563aa7581a3e2b04b58c54f0bfb6c93048
Vivu24 <93614868+Vivu24@users.noreply.github.com>
2023-06-05 17:25:37 +0200

    Hay melé al menos

--- Shoot Them All/Assets/Scripts/Knockback/KnockbackComponent.cs
+++ Shoot Them All/Assets/Scripts/Knockback/KnockbackComponent.cs
@@ -7,7 +7,7 @@ public class KnockbackComponent : MonoBehaviour
 {
     private Rigidbody2D _myRigidBody2D;
 
-    [SerializeField] private float powerRegulator;
+    //[SerializeField] private float powerRegulator;
     private Vector2 _impulseForce;
     private float _verticalImpulse;
     [Range(0f, 1f)] [SerializeField] float _proportionPercentagePerVerticalImpulse;     // Se puede cambiar a int
@@ -31,11 +31,12 @@ public class KnockbackComponent : MonoBehaviour
     {
         _verticalImpulse = _proportionPercentagePerVerticalImpulse * percentage;
         return 0.01f * percentage * (float)Math.Log(percentage);
-        return powerRegulator * percentage;
+        //return powerRegulator * percentage;
     }
 
     /// <summary>
-    /// Pasándole el objeto de la colisión y el porcentaje del receptor, llama a los métodos necesarios para realizar el knockback
+    /// Pasándole el objeto de la colisión y el porcentaje del receptor, llama a los métodos necesarios para realizar el knockback.
+    /// Ataque balas y dash
     /// </summary>
     /// <param name="collision"></param>
     /// <param name="percentage"></param>
@@ -45,6 +46,23 @@ public class KnockbackComponent : MonoBehaviour
         //igual hace falta quitar el delta time
         Debug.Log("Hay knockback");
         _impulseForce = ConvertDirection(collision) * ConvertPercentageToPower(percentage) + Vector2.up * _verticalImpulse;
+        _myRigidBody2D.velocity = Vector2.zero;
+        _myRigidBody2D.AddForce(_impulseForce, ForceMode2D.Impulse);
+        // Desactivar Input (por un periodo de tiempo) TODO
+        // iFrames (por un periodo de tiempo) TODO
+
+    }
+     /// <summary>
+     /// Knockback del ataque mele
+     /// </summary>
+     /// <param name="collision"></param>
+     /// <param name="percentage"></param>
+    public void Knockback(Vector2 direction, int percentage)
+    {
+        //Se puede usar el modo impulso para esto (LUIS)
+        //igual hace falta quitar el delta time
+        Debug.Log("Hay knockback");
+        _impulseForce = direction * ConvertPercentageToPower(percentage) + Vector2.up * _verticalImpulse;
         _myRigidBody2D.velocity = Vector2.zero;
         _myRigidBody2D.AddForce(_impulseForce, ForceMode2D.Impulse);
         // Desactivar Input (por un periodo de tiempo) TODO
