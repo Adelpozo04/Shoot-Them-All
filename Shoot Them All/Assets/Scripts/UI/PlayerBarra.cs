7b3bf3139cc966b3fdf7eedf7e79ad3b52949cb2
Adelpozo04 <addelp01@ucm.es>
2023-06-24 17:19:03 +0200

    Siguiendo con el inicio de la barra
    Se a completado el script de la barra que actualiza valores pero aun falta buscar una forma de instanciar el UI en tiempo de juego

--- Shoot Them All/Assets/Scripts/UI/PlayerBarra.cs
+++ Shoot Them All/Assets/Scripts/UI/PlayerBarra.cs
@@ -4,10 +4,28 @@ using UnityEngine;
 
 public class PlayerBarra : MonoBehaviour
 {
+    #region properties
+
+    [SerializeField] private GameObject _barraPrefab;
+    [SerializeField] private PercentageComponent _percentageJugador;
+    //Puntuacion rondas ganadas
+    //Color jugador
+    //Iconos armas y habilidades
+    [SerializeField] private Sprite _provisionalSprite; //SOLO PARA TESTEO 
+
+    private GameObject _barraJugador;
+
+    #endregion
+
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _barraJugador = Instantiate(_barraPrefab, transform.position, Quaternion.identity);
+        _barraJugador.GetComponent<BattleUIManager>().InicializacionBarra(_provisionalSprite, _provisionalSprite, _provisionalSprite, 2, 5, Color.yellow);
     }
 
     // Update is called once per frame
