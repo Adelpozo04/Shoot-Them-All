0659e07fe2cd5b695716bb5a19b0809cf105c1b1
Vivu24 <93614868+Vivu24@users.noreply.github.com>
2023-06-26 20:43:49 +0200

    Timer para cada ronda

---
+++ Shoot Them All/Assets/Scripts/UI/TimerComponent.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerComponent : MonoBehaviour
+{
+    [SerializeField] private Text _myTimerText;
+    [SerializeField] private float _timer = 75;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Actualizamos el tiempo del contador
+        if (_timer > 0)
+        {
+            _timer -= Time.deltaTime;
+        }
+        else
+        {
+            _timer = 0;
+        }
+
+        DisplayTimer(_timer);
+    }
+
+    /// <summary>
+    /// Método que muestra por pantalla el tiempo restante de partida, imporante EN PARTIDA
+    /// </summary>
+    /// <param name="myTime"></param>
+    private void DisplayTimer(float myTime)
+    {
+        if (myTime < 0)
+        {
+            myTime = 0;
+        }
+        else if(myTime > 0)
+        {
+            myTime += 1;
+        }
+
+        float minutes = Mathf.FloorToInt(myTime / 60);
+        float seconds = Mathf.FloorToInt(myTime % 60);
+
+        _myTimerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+    /// <summary>
+    /// Método para indicar la duración de cada ronda
+    /// </summary>
+    /// <param name="newTimer"></param>
+    public void SetRoundTimer(float newTimer)
+    {
+        _timer = newTimer;
+    }
+}
