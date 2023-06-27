e626365eda02b7382de9dfb300183ef3df3a0baf
Vivu24 <93614868+Vivu24@users.noreply.github.com>
2023-06-27 23:15:04 +0200

    Prueba Slow Motion
    Mirar en la escena de la Prueba de la Cámara Lenta.
    Se modifica el time scale simplemente

---
+++ Shoot Them All/Assets/Scripts/SlowMotionComponent.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowMotionComponent : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 0.2f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
